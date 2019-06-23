dddcf201f1ab9eddd01162e6e92b5f264677de88
Osmiogrzesznik <boleskok@gmail.com>
2019-06-23 04:12:35 +0100

    Added scripts test php/html file to practice what will be happening on the backend. Checked timezone in php and functionality needed for timebomb client to sync with server

--- getExplosionTime.php
+++ getExplosionTime.php
@@ -1,5 +1,37 @@
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
 <?php
-$timeNow = time();
-echo $timeNow;
+date_default_timezone_set('Europe/London');
+//$whenExplosion = date('Y-m-dTh:i:s');
+// $date = DateTime::createFromFormat('j-M-Y', '15-Feb-2009');
+// echo $date->format('Y-m-d');
+
+//comes from client - and later db
+$minutesSet = 2;
+$secondsSet = 30;
+//this is stored in db
+$explosionTimestamp = time() + $minutesSet * 60 + $secondsSet;
+
+
+$arr = array(
+	"explosionTimestamp" => $explosionTimestamp,
+	"asDateString" =>  date('Y-m-dTh:i:s',$explosionTimestamp),
+	"nowTimestamp" => time(),
+	"nowAsDateString" =>  date('Y-m-dTh:i:s',time())
+);
+$response = json_encode($arr);
+echo $response;
 
 ?>
+
+<br>
+	<script>document.write(~~(Date.now()/1000))</script>
+</body>
+</html>
