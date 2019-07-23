8ea376ed6281de57bc6989f1071359c1b68d814c
Osmiogrzesznik <boleslawkokocinski.bk@gmail.com>
2019-07-23 05:27:47 +0100

    label change in login from

--- ViewLoginForm.blade.php
+++ ViewLoginForm.blade.php
@@ -1,7 +1,7 @@
 <div class="centerpanel"><h2>Login</h2>
 
 <form method="post" action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" name="loginform">
-        <label for="login_input_username">Username (or email)</label>
+        <label for="login_input_username">Username</label>
         <input id="login_input_username" type="text" name="user_name" required />
         <label for="login_input_password">Password</label>
         <input id="login_input_password" type="password" name="user_password" required />
